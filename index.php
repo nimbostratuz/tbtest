cdd41d70577a093891e7513f3fff91740947c332
Isaac Newton <nimbostratuz@gmail.com>
2016-12-09 14:15:32 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php echo 'solo php'; ?>
+<?php
+
+$botToken = "304045275:AAGreyX5a8PJk4jbMOtXHhFWAXjPRa8tcws";
+$website = "https://api.telegram.org/bot".$botToken;
+
+$update = file_get_contents($website."/getupdates");
+
+print_r($update);
+
+
+//$a = 'How are you?';
+
+//if (strpos($a, 'are') !== false) {
+//    echo 'true';
+//}
+
+
+?>
