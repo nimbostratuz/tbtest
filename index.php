6d6561267cea09f02af43cea3592e842e67dca0f
Isaac Newton <nimbostratuz@gmail.com>
2016-12-10 19:13:14 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 
 $botToken = "304045275:AAGreyX5a8PJk4jbMOtXHhFWAXjPRa8tcws";
 $website = "https://api.telegram.org/bot".$botToken;
-
 $update = file_get_contents("php://input");
-
-print_r($update);
+$updateArray = json_decode($update, TRUE);
+$texto = $updateArray["result"][0]["message"]["text"];
+$chatId = $updateArray["result"][0]["message"]["chat"]["id"];
+file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=chupas");
 
 
 //$a = 'How are you?';
