87a68b720220b6a21f13dfcd78c063286ec7ae44
Isaac Newton <nimbostratuz@gmail.com>
2016-12-10 19:11:57 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $botToken = "304045275:AAGreyX5a8PJk4jbMOtXHhFWAXjPRa8tcws";
 $website = "https://api.telegram.org/bot".$botToken;
 
-$update = file_get_contents($website."/getupdates");
+$update = file_get_contents("php://input");
 
 print_r($update);
 
